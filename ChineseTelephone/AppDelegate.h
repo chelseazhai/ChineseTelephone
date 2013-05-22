4a6c09b69811d1cc2da3137c4c07246c9c04c2ea
chelsea zhai <chelsea.zhai@gmail.com>
2013-05-22 11:54:37 +0800

    Initial Commit

---
+++ ChineseTelephone/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ChineseTelephone
+//
+//  Created by Ares on 13-5-22.
+//  Copyright (c) 2013年 richitec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
